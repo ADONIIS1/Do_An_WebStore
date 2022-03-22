972667633758b3dd45fe1a9c19452cae427a1978
N-HaoTien <nht.it19@gmail.com>
2022-03-22 19:10:46 +0700

    Layout for Admin

--- WebStoreFZF/Controllers/AdminController.cs
+++ WebStoreFZF/Controllers/AdminController.cs
@@ -9,13 +9,27 @@ namespace WebStoreFZF.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult ThongKe()
         {
             return View();
         }
+
+        public ActionResult LoaiThietBi()
+        {
+            return View();
+        }
+        public ActionResult HangSanXuat()
+        {
+            return View();
+        }
+        public ActionResult MatHang()
+        {
+            return View();
+        }
+        public ActionResult KhachHang()
+        {
+            return View();
+        }
+
     }
 }
